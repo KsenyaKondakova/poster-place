c3a2a4add7030043701146024dc3fa2d63ccf481
ivan <ivanlisichkin@icloud.com>
2023-11-01 15:07:10 +0400

    feat:create a MongoDB api route

---
+++ admin/utils/db.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+async function connectToDatabase() {
+  const url = `mongodb://${process.env.USERNAME_MONGODB}:${process.env.PASSWORD_MONGODB}@${process.env.IP_MONGODB}/${process.env.DATABASE_MONGODB}`;
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db();
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    throw err;
+  }
+}
+
+module.exports = connectToDatabase;
