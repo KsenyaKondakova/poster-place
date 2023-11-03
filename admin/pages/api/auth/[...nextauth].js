1bb1c3a8847bec47e0ccafd000bb12d2dd2f1847
ivan <ivanlisichkin@icloud.com>
2023-11-03 13:34:45 +0400

    feat:add mongodb adapter

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import clientPromise from '@/lib/mongodb';
 import NextAuth from 'next-auth';
 import YandexProvider from 'next-auth/providers/yandex';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 
 export const authOptions = {
   providers: [
@@ -8,6 +10,7 @@ export const authOptions = {
       clientSecret: process.env.YANDEX_CLIENT_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export default NextAuth(authOptions);
