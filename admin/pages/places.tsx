bde477cc466aa3e05458e73c17f93701064d6abe
ivan <ivanlisichkin@icloud.com>
2023-11-08 18:28:42 +0400

    feat:add new place button

--- admin/pages/places.tsx
+++ admin/pages/places.tsx
@@ -1,8 +1,15 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 import React from 'react';
 
 function Places() {
-  return <Layout>Страница мест</Layout>;
+  return (
+    <Layout>
+      <Link href={'/places/new'} className="bg-nav-gray text-white p-2 px-4 rounded-xl">
+        Добавить новое заведение
+      </Link>
+    </Layout>
+  );
 }
 
 export default Places;
