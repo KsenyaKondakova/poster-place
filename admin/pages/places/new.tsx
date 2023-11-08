3148801630f3d32af1bd1f3a21529ceb9167124b
ivan <ivanlisichkin@icloud.com>
2023-11-08 19:29:45 +0400

    feat:add input field for descripton of the place and

--- admin/pages/places/new.tsx
+++ admin/pages/places/new.tsx
@@ -1,11 +1,13 @@
 import Layout from '@/components/Layout';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { json } from 'stream/consumers';
-
+import axios from 'axios';
 interface NewPlaceForm {
   placeName: string;
+  descriptionPlace: string;
 }
+
 function NewPlace() {
   const {
     register,
@@ -13,8 +15,8 @@ function NewPlace() {
     watch,
     formState: { errors },
   } = useForm<NewPlaceForm>();
-  const onSubmit = (data: NewPlaceForm) => {
-    console.log(JSON.stringify(data));
+  const onSubmit = async (data: NewPlaceForm) => {
+    await axios.post('/api/places', data);
   };
   return (
     <Layout>
@@ -23,6 +25,32 @@ function NewPlace() {
         <label className="label-form">Название заведения</label>
         <input className="form-input" {...register('placeName', { required: true })} />
         {errors?.placeName?.type === 'required' && (
+          <div className="flex gap-1 text-[#bf1650]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              />
+            </svg>
+            <p>Это поле должно быть заполнено</p>
+          </div>
+        )}
+        <label className="label-form">Описание заведения</label>
+
+        <textarea
+          className="form-input"
+          {...register('descriptionPlace', { required: true })}
+          cols={10}
+          rows={8}
+        />
+        {errors?.descriptionPlace?.type === 'required' && (
           <div className="flex gap-1 text-[#bf1650]">
             <svg
               xmlns="http://www.w3.org/2000/svg"
