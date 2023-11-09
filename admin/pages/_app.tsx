bb7b117dabd532de2f4d3fa18d015a021951c293
ivan <ivanlisichkin@icloud.com>
2023-11-09 16:24:04 +0400

    feat:add provider

--- admin/pages/_app.tsx
+++ admin/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   );
 }
