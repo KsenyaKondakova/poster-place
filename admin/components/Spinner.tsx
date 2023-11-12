62c6117e7902d681c114adfcd16d4efdb665e0ee
ivan <ivanlisichkin@icloud.com>
2023-11-12 14:56:49 +0400

    feat:add spinner component

---
+++ admin/components/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ScaleLoader } from 'react-spinners';
+
+function Spinner() {
+  return <ScaleLoader color="#2a2a2a" height={20} margin={2} radius={1} width={4} />;
+}
+
+export default Spinner;
