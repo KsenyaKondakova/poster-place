6b67ba9e5953a5803f738a8e673d127fa2864cf6
ivan <ivanlisichkin@icloud.com>
2023-11-12 14:57:09 +0400

    feat:change type

--- admin/models/Place.ts
+++ admin/models/Place.ts
@@ -3,11 +3,13 @@ import { Schema, model, models } from 'mongoose';
 interface IUser {
   title: string;
   description: string;
+  images: string[];
 }
 
 const placeSchema = new Schema<IUser>({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  images: [{ type: String }],
 });
 
 export const Place = models.Place || model('Place', placeSchema);
