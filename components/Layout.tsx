533cc3a66c58104fc794bf82d3978fd854482259
ivan <ivanlisichkin@icloud.com>
2023-12-03 17:39:48 +0400

    fix:add env

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,15 +3,13 @@ import Image from 'next/image';
 import Nav from '@/components/Nav';
 type LayoutProps = {
   children: React.ReactNode;
-  USERNAME_MONGODB: string;
 };
-function Layout({ children, USERNAME_MONGODB }: LayoutProps) {
+function Layout({ children }: LayoutProps) {
   const { data: session } = useSession();
   if (session) {
     return (
       <div className="flex bg-neutral-800 min-h-screen">
         <Nav />
-        <span>{USERNAME_MONGODB}</span>
         <div className="bg-yellow-100 flex-grow mt-2 mr-2 mb-2 rounded-xl p-4">
           {/* Signed in as {session.user?.email} <br />
           <button onClick={() => signOut()}>Sign out</button>
