eaf00c3fde0057ce07abaf522f8f6d27c25b4606
ivan <ivanlisichkin@icloud.com>
2023-12-06 17:08:51 +0400

    fix:delete test

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -26,7 +26,7 @@ function Layout({ children }: LayoutProps) {
         onClick={() => signIn('yandex')}
         className="flex items-center justify-center gap-2 text-black transition duration-300 ease-in-out bg-white p-3 rounded-md border-none px-4 transform hover:bg-red-200 hover:scale-105">
         <Image src="/Yandex_znak.png" width={30} height={30} alt="Picture of the author" />
-        <span>Войти с помощью Яндекс тест2</span>
+        <span>Войти с помощью Яндекс</span>
       </button>
     </div>
   );
