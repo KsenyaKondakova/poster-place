b06c79efbe06ea3bb2af08f67c1363c692dfbcc0
ivan <ivanlisichkin@icloud.com>
2023-12-15 21:18:23 +0400

    feat:change place schema

--- models/Place.ts
+++ models/Place.ts
@@ -1,15 +1,17 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose, { Schema, model, models } from 'mongoose';
 
 interface IUser {
   title: string;
   description: string;
   images: string[];
+  category?: IUser | mongoose.Types.ObjectId;
 }
 
 const placeSchema = new Schema<IUser>({
   title: { type: String, required: true },
   description: { type: String, required: true },
   images: [{ type: String }],
+  category: { type: mongoose.Types.ObjectId, ref: 'Category' },
 });
 
 export const Place = models?.Place || model('Place', placeSchema);
