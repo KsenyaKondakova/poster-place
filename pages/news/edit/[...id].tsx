bda249fcd6589349ab4ac8d8c661f6372dc6a30a
ivan <ivanlisichkin@icloud.com>
2023-12-26 17:46:24 +0400

    fix:change rendering news

--- pages/news/edit/[...id].tsx
+++ pages/news/edit/[...id].tsx
@@ -4,7 +4,7 @@ import { setNewsInfo } from '@/redux/slices/newsSlice';
 import { RootState } from '@/redux/store';
 import axios from 'axios';
 import { NextRouter, useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function EditNewsPage() {
@@ -12,20 +12,21 @@ function EditNewsPage() {
   const dispatch = useDispatch();
   const newsInfo = useSelector((state: RootState) => state.newsSlice.newsInfo);
   const id: string | string[] | undefined = router.query.id;
-
+  const [showForm, setShowForm] = useState<boolean>(false);
   useEffect(() => {
     if (!id) {
       return;
     }
     axios.get('/api/news?id=' + id).then((response) => {
       dispatch(setNewsInfo(response.data));
+      setShowForm(true);
     });
   }, [id]);
 
   return (
     <Layout>
       <h1 className="text-2xl mb-4">Редактировать новость</h1>
-      {<NewsForm {...newsInfo} />}
+      {showForm && newsInfo && <NewsForm {...newsInfo} />}
     </Layout>
   );
 }
