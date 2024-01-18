4949e058d43538a3a91a0878f9848c490bb2a94d
ivan <ivanlisichkin@icloud.com>
2024-01-18 14:22:07 +0400

    feat:add pagination to places

--- pages/places.tsx
+++ pages/places.tsx
@@ -1,7 +1,8 @@
 import PencilIcon from '@/assets/icons/PencilIcon';
 import TrashIcon from '@/assets/icons/TrashIcon';
 import Layout from '@/components/Layout';
-import { setPlaces } from '@/redux/slices/placeSlice';
+import PaginationComp from '@/components/Pagination';
+import { setOffset, setPage, setPageQty, setPlaces } from '@/redux/slices/placeSlice';
 import { RootState } from '@/redux/store';
 import axios from 'axios';
 import Link from 'next/link';
@@ -12,11 +13,16 @@ import { useDispatch, useSelector } from 'react-redux';
 function Places() {
   const dispatch = useDispatch();
   const places = useSelector((state: RootState) => state.placeSlice.placeList);
+  const limit = useSelector((state: RootState) => state.placeSlice.limit);
+  const offset = useSelector((state: RootState) => state.placeSlice.offset);
+  const page = useSelector((state: RootState) => state.placeSlice.page);
+  const pageQty = useSelector((state: RootState) => state.placeSlice.pageQty);
   useEffect(() => {
-    axios.get('/api/places').then((response) => {
-      dispatch(setPlaces(response.data));
+    axios.get(`/api/places?limit=${limit}&offset=${offset}`).then((response) => {
+      dispatch(setPlaces(response.data.places));
+      dispatch(setPageQty(response.data.totalPages));
     });
-  }, [dispatch]);
+  }, [page]);
   return (
     <Layout>
       <Link href={'/places/new'} className="submit-btn">
@@ -30,7 +36,7 @@ function Places() {
         <div className="flex flex-col">
           {places.map((place) => (
             <article
-              className="flex pt-4 pb-4 mx-4 border-b-2 border-gray-600 items-center"
+              className="flex pt-3 pb-3 mx-4 border-b-2 border-gray-600 items-center"
               key={place._id}>
               <span className="basis-2/3 text-orange-50">{place.title}</span>
               <div className="basis-1/3 flex items-center">
@@ -47,6 +53,7 @@ function Places() {
           ))}
         </div>
       </div>
+      <PaginationComp pageQty={pageQty} limit={limit} setOffset={setOffset} setPage={setPage} />
     </Layout>
   );
 }
