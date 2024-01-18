b4cf189d42ec2c7534a0fd5f31ef6f328b078b0f
ivan <ivanlisichkin@icloud.com>
2024-01-18 14:21:32 +0400

    feat:add pagination to categories

--- redux/slices/categorySlice.ts
+++ redux/slices/categorySlice.ts
@@ -4,8 +4,13 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: ICategoryState = {
   categoryList: [],
+  rootCategories: [],
   parentCategory: null,
   editedCategory: null,
+  limit: 10,
+  offset: 0,
+  page: 0,
+  pageQty: 0,
 };
 
 export const categorySlice = createSlice({
@@ -15,15 +20,39 @@ export const categorySlice = createSlice({
     setCategories: (state, action: PayloadAction<ICategorList[]>) => {
       state.categoryList = action.payload;
     },
+    setRootCategories: (state, action: PayloadAction<ICategorList[]>) => {
+      state.rootCategories = action.payload;
+    },
     setParentCategory: (state, action: PayloadAction<string | null>) => {
       state.parentCategory = action.payload;
     },
     setEditedCategory: (state, action: PayloadAction<ICategorList | string | null>) => {
       state.editedCategory = action.payload;
     },
+    setLimit: (state, action: PayloadAction<number>) => {
+      state.limit = action.payload;
+    },
+    setOffset: (state, action: PayloadAction<number>) => {
+      state.offset = action.payload;
+    },
+    setPage: (state, action: PayloadAction<number>) => {
+      state.page = action.payload;
+    },
+    setPageQty: (state, action: PayloadAction<number>) => {
+      state.pageQty = action.payload;
+    },
   },
 });
 
-export const { setCategories, setParentCategory, setEditedCategory } = categorySlice.actions;
+export const {
+  setCategories,
+  setParentCategory,
+  setEditedCategory,
+  setLimit,
+  setOffset,
+  setPage,
+  setPageQty,
+  setRootCategories,
+} = categorySlice.actions;
 
 export default categorySlice.reducer;
