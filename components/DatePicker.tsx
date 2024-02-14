79e7fe5ea1d990366ea6e21e9be03560efc1ceed
ivan <ivanlisichkin@icloud.com>
2024-02-14 15:22:40 +0400

    feat:add datepicker

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,22 +1,27 @@
 import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
-import { setDate } from '@/redux/slices/saleSlice';
-import { forwardRef, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
 
-const AirDatepickerReact = forwardRef((props: any, ref: any) => {
+import 'air-datepicker/air-datepicker.css';
+
+import { forwardRef, useEffect, useRef } from 'react';
+
+const AirDatepickerReact = forwardRef((props: any) => {
   const $input: any = useRef();
   const dp: any = useRef();
-  const dispatch = useDispatch();
+
   useEffect(() => {
     dp.current = new AirDatepicker($input.current, {
-      onSelect: ({ _, formattedDate, __ }: any) => {
-        dispatch(setDate(formattedDate));
+      onSelect: ({ formattedDate }: any) => {
+        props.setDate(formattedDate);
         if (props.onSelect) {
-          props.onSelect({ date: formattedDate, formattedDate, datepicker: dp.current });
+          props.onSelect({
+            date: formattedDate,
+            formattedDate,
+            datepicker: dp.current,
+          });
         }
       },
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -25,13 +30,14 @@ const AirDatepickerReact = forwardRef((props: any, ref: any) => {
 
   return (
     <input
+      autoComplete="off"
       id={props.id}
       className={props.className}
       type={props.type}
       ref={$input}
       placeholder={props.placeholder}
       value={props.valueDate}
-      onChange={(ev) => dispatch(setDate(ev.target.value))}
+      onChange={(ev) => props.setDate(ev.target.value)}
     />
   );
 });
