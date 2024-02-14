784183d487c2a62513bcc187b510462d7b78a6cc
ivan <ivanlisichkin@icloud.com>
2024-02-14 15:21:34 +0400

    feat:add icons

--- assets/icons/NewsIcon.tsx
+++ assets/icons/NewsIcon.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import type { FC } from 'react';
 
-const NewsIcon: React.FC = () => (
+const NewsIcon: FC = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
