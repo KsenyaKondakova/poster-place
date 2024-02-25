e8f991f50005f417889b19786fd6e92a40c979b2
ivan <ivanlisichkin@icloud.com>
2024-02-25 16:47:50 +0400

    fix:debug news

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -2,46 +2,56 @@ import AirDatepicker from 'air-datepicker';
 
 import 'air-datepicker/air-datepicker.css';
 
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 
-const AirDatepickerReact = forwardRef((props: any) => {
-  const $input: any = useRef();
-  const dp: any = useRef();
+interface AirDatepickerProps {
+  id?: string;
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  valueDate?: string;
+  setDate: (date: string) => void;
+}
+const AirDatepickerReact = forwardRef<HTMLInputElement, AirDatepickerProps>(
+  (props, ref) => {
+    const [dateValue, setDateValue] = useState('');
 
-  useEffect(() => {
-    dp.current = new AirDatepicker($input.current, {
-      onSelect: ({ formattedDate }: any) => {
-        props.setDate(formattedDate);
-        if (props.onSelect) {
-          props.onSelect({
-            date: formattedDate,
-            formattedDate,
-            datepicker: dp.current,
-          });
-        }
-      },
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const $input = useRef();
+    const dp = useRef();
 
-  useEffect(() => {
-    dp.current.update();
-  }, []);
-  useEffect(() => {
-    dp.current.update();
-  }, [props.valueDate]);
-  return (
-    <input
-      autoComplete="off"
-      id={props.id}
-      className={props.className}
-      type={props.type}
-      ref={$input}
-      placeholder={props.placeholder}
-      value={props.valueDate}
-      onChange={(ev) => props.setDate(ev.target.value)}
-    />
-  );
-});
+    useEffect(() => {
+      if (props.valueDate) {
+        setDateValue(props.valueDate);
+      }
+    }, [props.valueDate]);
+
+    useEffect(() => {
+      dp.current = new AirDatepicker($input.current, {
+        onSelect: ({ formattedDate }) => {
+          setDateValue(formattedDate);
+          props.setDate(formattedDate);
+        },
+      });
+    }, []);
+
+    useEffect(() => {
+      dp.current.update();
+    }, [dateValue]);
+
+    return (
+      <input
+        autoComplete="off"
+        id={props.id}
+        className={props.className}
+        type={props.type}
+        ref={ref || $input}
+        placeholder={props.placeholder}
+        value={props.valueDate ? props.valueDate : dateValue}
+        onChange={(ev) => setDateValue(ev.target.value)}
+      />
+    );
+  },
+);
+
 AirDatepickerReact.displayName = 'AirDatepickerReact';
 export default AirDatepickerReact;
